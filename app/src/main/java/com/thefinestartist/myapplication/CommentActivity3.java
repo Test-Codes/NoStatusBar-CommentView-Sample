6f9036307f376591c700ce2fcd07da28a411e0c0
Leonardo Taehwan Kim <leonardo@thefinestartist.com>
2016-03-22 09:44:24 +0900

    Remove useless codes

--- app/src/main/java/com/thefinestartist/myapplication/CommentActivity3.java
+++ app/src/main/java/com/thefinestartist/myapplication/CommentActivity3.java
@@ -3,8 +3,8 @@ package com.thefinestartist.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ScrollView;
 
@@ -16,12 +16,11 @@ import com.thefinestartist.utils.ui.DisplayUtil;
 /**
  * Created by TheFinestArtist on 3/21/16.
  */
-public class CommentActivity3 extends AppCompatActivity implements CustomEditText.OnKeyboardListener, View.OnClickListener, SoftInputModeHelper3.SoftKeyboardListener {
+public class CommentActivity3 extends AppCompatActivity implements SoftInputModeHelper3.SoftKeyboardListener {
 
     FrameLayout layout;
     FrameLayout mainView;
-    FrameLayout editView;
-    CustomEditText customEditText;
+    EditText editText;
     boolean globalLayoutConsumed = false;
 
     int mainViewHeight = DisplayUtil.getHeight() - Unit.dpToPx(56);
@@ -34,57 +33,33 @@ public class CommentActivity3 extends AppCompatActivity implements CustomEditTex
         setSupportActionBar(toolbar);
         layout = (FrameLayout) findViewById(R.id.layout);
         mainView = (FrameLayout) findViewById(R.id.mainView);
-        editView = (FrameLayout) findViewById(R.id.editView);
-        customEditText = (CustomEditText) findViewById(R.id.editText);
+        editText = (EditText) findViewById(R.id.editText);
 
-        L.e("mainViewHeight: " + mainViewHeight);
         mainView.setMinimumHeight(mainViewHeight);
         mainView.setLayoutParams(new ScrollView.LayoutParams(DisplayUtil.getWidth(), mainViewHeight));
-        editView.setLayoutParams(new FrameLayout.LayoutParams(DisplayUtil.getWidth(), mainViewHeight));
 
-        customEditText.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+        editText.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                L.showThreadInfo(true).e("onGlobalLayout");
                 if (!globalLayoutConsumed) {
-                    ViewHelper.setTranslationY(customEditText, mainViewHeight - customEditText.getHeight());
+                    ViewHelper.setTranslationY(editText, mainViewHeight - editText.getHeight());
                     globalLayoutConsumed = !globalLayoutConsumed;
                 }
             }
         });
-
-        customEditText.setOnKeyboardListener(this);
-        customEditText.setOnClickListener(this);
         SoftInputModeHelper3.assistActivity(this, this);
     }
 
     @Override
     public void onStateChanged(boolean showing, int keyboardHeight) {
-        L.e("mainView: " + mainView.getHeight());
-        L.e("customEditText: " + customEditText.getHeight());
-        L.e("onStateChanged:" + showing + ", keyboardHeight:" + keyboardHeight);
         if (showing) {
-            int mainViewHeight = DisplayUtil.getHeight() - Unit.dpToPx(56);
-            L.e("mainViewHeight: " + mainViewHeight);
             mainView.setMinimumHeight(mainViewHeight - keyboardHeight);
             mainView.setLayoutParams(new ScrollView.LayoutParams(DisplayUtil.getWidth(), mainViewHeight - keyboardHeight));
-            editView.setLayoutParams(new FrameLayout.LayoutParams(DisplayUtil.getWidth(), mainViewHeight - keyboardHeight));
-            ViewHelper.setTranslationY(customEditText, mainViewHeight - customEditText.getHeight() - keyboardHeight);
+            ViewHelper.setTranslationY(editText, mainViewHeight - editText.getHeight() - keyboardHeight);
         } else {
             mainView.setMinimumHeight(mainViewHeight);
             mainView.setLayoutParams(new ScrollView.LayoutParams(DisplayUtil.getWidth(), mainViewHeight));
-            editView.setLayoutParams(new FrameLayout.LayoutParams(DisplayUtil.getWidth(), mainViewHeight));
-            ViewHelper.setTranslationY(customEditText, mainViewHeight - customEditText.getHeight());
+            ViewHelper.setTranslationY(editText, mainViewHeight - editText.getHeight());
         }
-    }
-
-    @Override
-    public void onKeyboardHidden() {
-        L.e("onKeyboardHidden");
-    }
-
-    @Override
-    public void onClick(View v) {
-        L.e("onClick");
     }
 }
